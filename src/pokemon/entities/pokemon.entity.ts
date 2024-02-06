4b2605fc5f66ac07665247d81a896d0c5800ed87
Juanita Velez <jvalentina.velez@gmail.com>
2024-02-05 21:51:05 -0500

    post mongodb

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,24 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+//decorador para indicar que es esquema de base de datos
+@Schema()
+export class Pokemon extends Document{
+
+    //id: string // Mongo lo entrega
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    no: number;
+}
+
+//exportar schema basado en clase definida
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon );
